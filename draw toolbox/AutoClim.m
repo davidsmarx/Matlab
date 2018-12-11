42ffda5afe79120c3d800ea274fd3ae5cc1e41f1
dmarx <David.S.Marx@jpl.nasa.gov>
2018-12-11 09:33:39 -0800

    AutoClim.m -- revised to use CheckOption and check that clim is increasing

--- draw toolbox/AutoClim.m
+++ draw toolbox/AutoClim.m
@@ -7,25 +7,37 @@ function clim = AutoClim(A, varargin)
 %   'symmetric' (i.e. two-sided and symmetric)
 
 % options
+bOnesided = CheckOption('one-sided',true,varargin{:});
+bSymmetric = CheckOption('symmetric',false,varargin{:});
+
+if bOnesided && bSymmetric,
+    % doesn't make sense
+    warning(['cannot be both one-sided and symmetric, setting to symmetric']);
+    bOnesided = false;
+end
 
 % for complex numbers, sort sorts the abs()
 asort = sort(A(:),'ascend');
 amax = asort(ceil(0.99*length(asort)));
 amin = asort(floor(0.01*length(asort)));
 
-% default is one-sided
-clim = [0 amax];
+% check result is valid
+if amax - amin <= 0,
+    warning(['clim = ' num2str([amin amax]) '; clim must be monotonic increasing. Switching to [min max].']);
+    amin = min(A(:));
+    amax = max(A(:));
+end
 
-if any(ismember(varargin, 'one-sided')),
+
+if bOnesided,
     clim = [0 amax];
 
-elseif any(ismember(varargin, 'two-sided')) ...
-        || any(ismember(varargin, 'symmetric')),
+elseif bSymmetric,
+    % two-sided, symmetric around a==0
+    clim = max(abs([amin amax]))*[-1 1];
+    
+else,
+    % two-sided not symmetric
     clim = [amin amax];
 
 end
-
-if any(ismember(varargin, 'symmetric')),
-    clim = max(abs(clim))*[-1 1];    
-end
-
