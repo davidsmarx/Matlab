c9c01aaf679a3c81a39566a7c809a07236e45714
dmarx <David.S.Marx@jpl.nasa.gov>
2019-03-05 13:57:33 -0800

    added mcb_hlc to CGS new num2column, hopefully more general to use with Excel

--- Excel ActiveX toolbox/num2column.m
+++ Excel ActiveX toolbox/num2column.m
@@ -1,15 +1,30 @@
-function column = num2column(num)
+function strCol = num2column(num)
 % column = num2column(num)
 
 if num <= 0, error('column starts with 1'); end
+if num == 1, strCol = 'A'; return, end
 
-first = floor((num-1)/26);
-sec   = rem((num-1),26) + 1;
+% number of digits
+ndig = ceil(log(num)/log(26));
 
-if first < 1,
-    column = 'A' + sec - 1;
-else,
-    column = [ 'A' + first - 1, 'A' + sec - 1 ];
+numrem = num-1;
+for ii = 1:ndig,
+    bb = (26.^(ndig-ii));
+    column(ii) = floor((numrem)/bb);
+    numrem = rem((numrem),bb);
+    sCol(ii) = 'A' + column(ii);
 end
 
-column = char(column);
+disp(column);
+
+% 
+% first = floor((num-1)/26);
+% sec   = rem((num-1),26) + 1;
+% 
+% if first < 1,
+%     column = 'A' + sec - 1;
+% else,
+%     column = [ 'A' + first - 1, 'A' + sec - 1 ];
+% end
+% 
+strCol = char(sCol);
