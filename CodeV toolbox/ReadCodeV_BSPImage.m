1b8cec1e847ed478003ae4a595ed9e5ae1f7711b
Marx <dmarx1@ndc.nasa.gov>
2019-07-12 07:05:50 -0700

    Code V BSP stuff

---
+++ CodeV toolbox/ReadCodeV_BSPImage.m
@@ -0,0 +1,82 @@
+function [Im, sParms] = ReadCodeV_BSPImage(fn)
+% [Im, sParms] = ReadCodeVIntensityTxt(fn)
+
+U = CConstants;
+
+if nargin == 0,
+    [fn, pn] = uigetfile({'*.txt'});
+    fn = [pn '/' fn];
+end
+
+Im = [];
+
+fid = fopen(fn,'rt');
+if isequal(fid,-1), error(['error opening file: ' fn]); end
+
+ltmp = fgetl(fid);
+
+while ~feof(fid),
+    if isempty(ltmp),
+        ltmp = fgetl(fid);
+        continue
+    end
+    
+    wordparse = textscan(ltmp,'%s');
+    
+    switch wordparse{1}{1},
+        
+        case 'These'
+            % These data represent the Intensity at surface 48
+            % These data represent the Complex Field at surface 48
+            sParms.DataType = wordparse{1}{5};
+            sParms.AtSurface = wordparse{1}{end};
+            
+        case 'Zoom'
+            A = textscan(ltmp,'Zoom Position: %d Field Number: %d Wavelength (nm.):	%f');
+            [sParms.zoom, sParms.FieldNum, wvtmp] = deal(A{:});
+            sParms.Wavelength = wvtmp * U.NM;
+            
+        case 'Defocus:'
+            
+        case 'Index'
+            
+        case 'Fraction'
+            
+        case 'Beam'
+            
+        case 'Coordinate'
+            
+        case 'Orientation'
+            
+        case 'Offset'
+            
+        case 'Grid'
+            A = textscan(ltmp,'Grid spacing: %f %s %f');
+            [dx, dy] = deal(A{[1 3]});
+            units = A{2}{1}; % assum 'mm' for now
+            sParms.dx = dx*U.MM;
+            sParms.dy = dy*U.MM;
+                        
+        case 'Array'
+            sParms.Nr = str2double(wordparse{1}{end-1});
+            sParms.Nc = str2double(wordparse{1}{end});
+            
+            % Im data follows
+            if strcmpi(sParms.DataType, 'Intensity'),
+                Im = fscanf(fid,'%f',[sParms.Nr sParms.Nc]);
+            elseif strcmpi(sParms.DataType, 'Complex'),
+                Im = fscanf(fid,'%f',[2*sParms.Nc sParms.Nr]); % fscanf is in column order
+                Im = Im(1:2:end,:) + 1i*Im(2:2:end,:);
+            end
+            Im = Im.'; % back to row, column
+            
+        otherwise
+            
+    end % switch
+
+    ltmp = fgetl(fid);
+    
+end % while ~feof
+
+end % main
+
