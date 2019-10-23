01a457cc23a54718f3857f7a8febfe4c944c9aa2
dmarx <David.S.Marx@jpl.nasa.gov>
2019-10-23 13:57:32 -0700

    CRunData.m updated DisplayDEfields and DisplayEfields GenerateEFCReport.m plots rms|dE|^2 v it# if DisplayDEfields is run

--- HCIT utils toolbox/GenerateEFCReport.m
+++ HCIT utils toolbox/GenerateEFCReport.m
@@ -43,8 +43,8 @@ catch
     clear Sppt; Sppt = [];
 end
 
-% open PowerPoint if necessary
-if isempty(Sppt) && ispc,
+% open PowerPoint if necessary, and plots are requested
+if isempty(Sppt) && ispc && ~isempty(varargin),
     Sppt = Cppt(ppt_fn);
 end
 
@@ -61,6 +61,7 @@ else
     error(['listItnum type error: ' class(listItnum)]);
 end
 
+% call the plotting methods
 listHfig = {};
 for iplot = 1:length(varargin),
     if iscell(varargin{iplot}),
@@ -88,9 +89,10 @@ more on
 
 end % main
 
-function hfig = CreaetePlots(S, sDisplayFun, Sppt, varargin)
+function [hfig, hax, sMetrics] = CreaetePlots(S, sDisplayFun, Sppt, varargin)
     % create the plots
     % some plots are differential
+    % some plots we also plot metrics v itnum
 
     save_pn = CheckOption('save_pn', ['./' sDisplayFun '/'], varargin{:}); % is ~ispc
     figheight = CheckOption('figheight', 700, varargin{:}); % for ppt display
@@ -114,7 +116,9 @@ function hfig = CreaetePlots(S, sDisplayFun, Sppt, varargin)
         case listDiff
             %if any(strcmp(sDisplayFun, listDiff)),
             for ii = 1:N-1,
-                hfig = S(ii+1).(sDisplayFun)(S(ii), varargin{:},'hfig',hfig);
+                [hfig, hax, sMtmp] = S(ii+1).(sDisplayFun)(S(ii), varargin{:},'hfig',hfig);
+                sMetrics(ii) = sMtmp;
+                
                 figscale = CalcFigscale(hfig, figheight);
                 set(hfig, 'Position', figscale*get(hfig,'position'));
                 if ispc,
@@ -125,11 +129,32 @@ function hfig = CreaetePlots(S, sDisplayFun, Sppt, varargin)
                 end
             end % for ii iter
         
+            if strcmp(sMtmp(1).type, 'dEfields'),
+                nw = S(1).NofW; % for convenience
+                figure, 
+                hh = semilogy([S(2:N).iter], [sMetrics.rmsdE_t].^2, '-', ...
+                    [S(2:N).iter], mean([sMetrics.rmsdE_t].^2, 1), '-', ...
+                    [S(2:N).iter], [sMetrics.rmsdE_m].^2, '--', ...
+                    [S(2:N).iter], mean([sMetrics.rmsdE_m].^2, 1), '--');
+                
+                grid on
+                set(hh(nw+1),'linewidth',2)
+                set(hh(end), 'linewidth',2)
+                %set(gca,'ylim',get(hax(1),'clim'))
+                %set(gca,'ylim',[1e-9 1e-6])
+                wvstrtmp = num2str(S(1).NKTcenter(1:nw)'/S(1).NM);
+                tbwvstrtmp = char(strcat('TB', {' '}, wvstrtmp, 'nm'));
+                mowvstrtmp = char(strcat('Model', {' '}, wvstrtmp, 'nm'));
+                legend(char(tbwvstrtmp, 'TB-mean', mowvstrtmp, 'Model-Mean'))
+                xlabel('Iteration #')
+                ylabel('mean |\DeltaE|^2')
+            end
+            
         case 'DisplayCEfields'
 
             for ii = 1:N-1,
                 [hfig, hax, sCtmp] = S(ii+1).(sDisplayFun)(S(ii), varargin{:},'hfig',hfig);
-                sCmetric(ii) = sCtmp;
+                sMetrics(ii) = sCtmp;
                 
                 figscale = CalcFigscale(hfig, figheight);
                 set(hfig, 'Position', figscale*get(hfig,'position'));
@@ -141,10 +166,11 @@ function hfig = CreaetePlots(S, sDisplayFun, Sppt, varargin)
                 end
             end % for ii iter
 
-            figure, plotampphase([S(2:N).iter], [sCmetric.CC],...
+            figure, plotampphase([S(2:N).iter], [sMetrics.CC],...
                 'xlabel','Iteration #','title','\DeltaE Testbed Model Correlation (CC)');
             
         otherwise, % one call per iteration
+            sMetrics = struct;
             for ii = 1:N,
                 hfig = S(ii).(sDisplayFun)(varargin{:},'hfig',hfig);
                 figscale = CalcFigscale(hfig, figheight);
@@ -185,7 +211,9 @@ function [hfig, hax, probeh] = PlotProbeh(S)
 end % PlotProbeh
 
 function [hfig, hax, rmsdDMv] = PlotRMSdDMv(listS, varargin)
+    % [hfig, hax, rmsdDMv] = PlotRMSdDMv(listS, varargin)
 
+    % read all the DMv cubes
     for ii = 1:length(listS),
         if isempty(listS(ii).DMvCube)
             listS(ii).ReadDMvCube;
@@ -194,24 +222,33 @@ function [hfig, hax, rmsdDMv] = PlotRMSdDMv(listS, varargin)
     
     Ndm = length(listS(1).DMvCube);
     
+    % select the unprobed DMv from each cube
     for ii = 1:length(listS),
         for idm = 1:Ndm
             DMvtmp{idm} = squeeze(listS(ii).DMvCube{idm}(:,:,1));
         end
-        listDMv{ii} = [DMvtmp{:}];
+        listDMv(ii,:) = DMvtmp; % listDMv is cell array
     end
     
     % rms difference
-    rmsdDMv = zeros(length(listS)-1,1);
+    rmsdDMv = zeros(length(listS)-1,Ndm);
     for ii = 1:length(listS)-1
-        iuse = abs(listDMv{ii+1}) > 0 & abs(listDMv{ii}) > 0;
-        rmsdDMv(ii) = rms(listDMv{ii+1}(iuse) - listDMv{ii}(iuse));
+        for idm = 1:Ndm,
+            iuse = abs(listDMv{ii+1,idm}) > 0 & abs(listDMv{ii,idm}) > 0;
+            rmsdDMv(ii,idm) = rms(listDMv{ii+1,idm}(iuse) - listDMv{ii,idm}(iuse));
+        end
     end
     
     hfig = figure;
-    plot([listS(2:end).iter].', rmsdDMv), grid
+    hh = plot([listS(2:end).iter].', rmsdDMv, '-', [listS(2:end).iter].', mean(rmsdDMv,2), '--');
+    set(hh,'LineWidth', 1.0)
+    grid on
     xlabel('Iteration #')
     ylabel('rms \Delta Vmu')
+    for idm = 1:Ndm,
+        legstr{idm} = ['DM ' num2str(idm)];
+    end
+    legend(legstr{:}, 'Mean')
     hax = gca;
     
 end % PlotRMSdDMv
