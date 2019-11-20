e164b8d57a89bfa6bcce5bbb54b9a7072aa1a45b
David Marx <david.s.marx@jpl.nasa.gov>
2019-11-20 11:11:32 -0800

    sortdata.m, the additional arrays can be n-d arrays, the first dimension is sorted

--- functions toolbox/sortdata.m
+++ functions toolbox/sortdata.m
@@ -8,18 +8,39 @@ function varargout = sortdata(cellA,varargin)
 % [a, isort] = sort(a, ...), and apply sort order
 % b = B(isort); c = C(isort); ...
 %
+% A must be 1-d
+% if B, C, etc. ndims >= 2, sort B(isort,:)
 %
 % varargin are optional arguments passed to sort(a, ...)
 %
+
+sortdim = CheckOption('sortdim', 1, varargin{:}); % only know how to do 1 for now
 
 [varargout{1}, isort] = sort(cellA{1}, varargin{:});
 
 nsort = min(nargout, length(cellA));
 for ii = 2:nsort,
-    varargout{ii} = cellA{ii}(isort);
+    if isvector(cellA{ii}),
+        varargout{ii} = cellA{ii}(isort);
+    else
+        varargout{ii} = ndimsort(cellA{ii}, isort, sortdim);
+    end
 end
 
 if nargout == nsort + 1,
     varargout{nargout} = isort;
 end
+
+
+function Bsort = ndimsort(B, isort, sortdim)
+% only know sortdim = 1 for now
+sizeB = size(B);
+%ndimsB = ndims(B);
+
+Br = reshape(B, [sizeB(1) prod(sizeB(2:end))]);
+Brsort = Br(isort, :);
+Bsort = reshape(Brsort, sizeB);
+
+
+
 
