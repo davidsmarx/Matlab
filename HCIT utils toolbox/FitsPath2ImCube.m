d71acb13ee3a076e0993ce9e4f1c4e7afe50d8d6
David Marx <david.s.marx@jpl.nasa.gov>
2020-01-21 14:36:08 -0800

    update to FitsPath2ImCube.m

--- HCIT utils toolbox/FitsPath2ImCube.m
+++ HCIT utils toolbox/FitsPath2ImCube.m
@@ -4,10 +4,20 @@ function [ImCube, hfig, hax] = FitsPath2ImCube(pn, varargin)
 % simple routine to collect all the fits files in a subdir
 % for example, making an image cube out of all the fits files in a
 % phase retrieval subdir
-
-% options
-
 %
+% options
+%   'display', (true) or false
+%   'x', 'y',  (0, 0), arguments into ImageCube if display, default 0-offset
+%
+
+bDisplay = CheckOption('display', true, varargin{:});
+xDisplay = CheckOption('x', 0, varargin{:});
+yDisplay = CheckOption('y', 0, varargin{:});
+
+% initialize return vals
+hfig = [];
+hax = [];
+ 
 listfn = dir(PathTranslator([pn '/*.fits']));
 Nf = length(listfn);
 
@@ -31,6 +41,9 @@ for ii = 1:Nf
 end
 
 
-figure, [hfig, hax, sUserData] = ImageCube(ImCube, camz, 'x', 0, 'y', 0);
+if bDisplay,
+    figure, [hfig, hax, sUserData] = ImageCube(ImCube, camz, 'x', xDisplay, 'y', yDisplay);
+end
+
 
 
