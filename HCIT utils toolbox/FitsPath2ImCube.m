0c33c849ae57907d3236ab9b6233ad2d26263de9
dmarx <David.S.Marx@jpl.nasa.gov>
2020-01-22 15:55:16 -0800

    incremental

--- HCIT utils toolbox/FitsPath2ImCube.m
+++ HCIT utils toolbox/FitsPath2ImCube.m
@@ -6,6 +6,11 @@ function [ImCube, hfig, hax] = FitsPath2ImCube(pn, varargin)
 % phase retrieval subdir
 
 % options
+plottype = CheckOption('plottype', 'cube', varargin{:}); % 'spread'
+plotx = CheckOption('x', 0, varargin{:});
+ploty = CheckOption('y', 0, varargin{:});
+xlim = CheckOption('xlim', [], varargin{:});
+ylim = CheckOption('ylim', [], varargin{:});
 
 %
 listfn = dir(PathTranslator([pn '/*.fits']));
@@ -14,8 +19,9 @@ Nf = length(listfn);
 % get image size
 imtmp = fitsread(PathTranslator([pn '/' listfn(1).name]),'image');
 finfo = fitsinfo(PathTranslator([pn '/' listfn(1).name]));
+[Nr, Nc] = size(imtmp);
 
-ImCube = zeros([Nf size(imtmp)]);
+ImCube = zeros([Nf Nr Nc]);
 camz = zeros(Nf,1);
 
 ImCube(1,:,:) = imtmp;
@@ -30,7 +36,63 @@ for ii = 1:Nf
     camz(ii) = FitsGetKeywordVal(finfo.PrimaryData.Keywords,'camz');
 end
 
+switch lower(plottype),
+    case 'cube',
+        figure,
+        [hfig, hax, sUserData] = ImageCube(ImCube, camz,...
+            'x', plotx, 'y', ploty);
+        
+    case 'spread'
+        figure
+        [hfig, hax] = PlotSpread;
+        
+    otherwise,
+        error(['unknown plottype ' plottype]);
+end
 
-figure, [hfig, hax, sUserData] = ImageCube(ImCube, camz, 'x', 0, 'y', 0);
+return % end of main
 
+%%%%%%%%%%%%%% nested plot functions
+    % nested function to create panoramic spread plot has all the options
+    % already in scope
+    function [hfig, hax] = PlotSpread
+        hfig = gcf;
+        
+        % x, y axes
+        if isscalar(plotx),
+            x = (plotx:(Nc-plotx-1))';
+        else
+            x = plotx;
+        end
+        if isscalar(ploty),
+            y = (ploty:(Nr-ploty-1))';
+        else
+            y = ploty;
+        end
+        
+        % apply xlim, ylim directly to ImCube
+        if ~isempty(xlim)
+            ix = x>=xlim(1) & x<=xlim(2);
+            ImCube = ImCube(:,:,ix);
+            x = x(ix);
+        end
+        if ~isempty(ylim)
+            iy = (y>=ylim(1) & y<=ylim(2));
+            ImCube = ImCube(:,iy,:);
+            y = y(iy);
+        end
+        
+        % unfold cube
+        nr = length(y); nc = length(x);
+        img = zeros(nr, Nf*nc);
+        for isl = 1:Nf,
+            img(:, (isl-1)*nc + (1:nc)) = squeeze(ImCube(isl,:,:));
+        end %
+        imageschcit(img)
+        hax = gca;
+        title(pwd2titlestr(pn),'fontsize',14)
+        
+    end % PlotSpread
+
+end % main
 
