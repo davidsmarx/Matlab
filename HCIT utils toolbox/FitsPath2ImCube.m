e10e74642de79b5143f6cea35023dd5d5bd6f2c4
dmarx <David.S.Marx@jpl.nasa.gov>
2020-02-05 23:17:16 -0800

    more options for FitsPath2ImCube

--- HCIT utils toolbox/FitsPath2ImCube.m
+++ HCIT utils toolbox/FitsPath2ImCube.m
@@ -1,9 +1,18 @@
 function [ImCube, hfig, hax] = FitsPath2ImCube(pn, varargin)
-% ImCube = FitsPath2ImCube(pn)
+% ImCube = FitsPath2ImCube(pn, options)
 %
 % simple routine to collect all the fits files in a subdir
 % for example, making an image cube out of all the fits files in a
 % phase retrieval subdir
+%
+% options:
+% plottype = CheckOption('plottype', 'cube', varargin{:}); % 'spread'
+% plotx = CheckOption('x', 0, varargin{:});
+% ploty = CheckOption('y', 0, varargin{:});
+% xlim = CheckOption('xlim', [], varargin{:});
+% ylim = CheckOption('ylim', [], varargin{:});
+% scale = CheckOption('scale', 'linear', varargin{:});
+
 
 % options
 plottype = CheckOption('plottype', 'cube', varargin{:}); % 'spread'
@@ -11,6 +20,7 @@ plotx = CheckOption('x', 0, varargin{:});
 ploty = CheckOption('y', 0, varargin{:});
 xlim = CheckOption('xlim', [], varargin{:});
 ylim = CheckOption('ylim', [], varargin{:});
+scale = CheckOption('scale', 'linear', varargin{:});
 
 %
 listfn = dir(PathTranslator([pn '/*.fits']));
@@ -35,6 +45,18 @@ for ii = 1:Nf
     ImCube(ii,:,:) = imtmp;
     camz(ii) = FitsGetKeywordVal(finfo.PrimaryData.Keywords,'camz');
 end
+
+% apply scale (stretch)
+switch lower(scale),
+    case 'linear',
+        % do nothing
+    case 'log'
+        ImCube = log10(ImCube);
+        
+    otherwise,
+        error(['unknown scale: ' scale]);
+end
+
 
 switch lower(plottype),
     case 'cube',
