2ef08bb47d6ecf07894d8ce00a3021b01699c3dc
David Marx <david.s.marx@jpl.nasa.gov>
2021-12-15 09:22:51 -0800

    modified:   startup.m new pyenv

--- startup.m
+++ startup.m
@@ -13,15 +13,17 @@ clear;
 constants;
 unitsdefinitions;
 
-% python path
-[pyver, pyexe, isloaded] = pyversion;
-if ~isempty(pyver),
-    pypath = py.sys.path;
-    pypath.append('/home/dmarx/src/Falco-jpl/FALCO-python/falco')
-    pypath.append('/home/dmarx/src/Falco-jpl/FALCO-python/falco/models')
-    pypath.append('/home/dmarx/src/python_toolbox')
-else,
-    warning('Python not found');
-end
-    
+PYENV = pyenv('Version', 'C:\ProgramData\Anaconda3\python.exe')
+% % python path
+% [pyver, pyexe, isloaded] = pyversion;
+% if ~isempty(pyver),
+%     pypath = py.sys.path;
+%     pypath.append('/home/dmarx/src/Falco-jpl/FALCO-python/falco')
+%     pypath.append('/home/dmarx/src/Falco-jpl/FALCO-python/falco/models')
+%     pypath.append('/home/dmarx/src/python_toolbox')
+% else,
+%     warning('Python not found');
+% end
+
+
 more on
