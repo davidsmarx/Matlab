7b985e7e08ac1c62f41425d8e475b154f37739ca
David Marx <david.s.marx@jpl.nasa.gov>
2024-11-04 09:52:19 -0800

    changed from energy density to spectral radiance: 	modified:   physical optics toolbox/blackbodyspectrum.m

--- physical optics toolbox/blackbodyspectrum.m
+++ physical optics toolbox/blackbodyspectrum.m
@@ -1,7 +1,9 @@
 function U = blackbodyspectrum(lam,tt)
 % U = blackbodyspectrum(lam,T)
 %
-% Spectral energy density, U(?,T) = 8?hc?^-5 /  ( e^(hc/?kT)-1 ) 
+% Spectral energy density [J/m^4], U(lam,T) = 8 pi h c lam^-5 /  ( e^(h c/(lam kT)) - 1 ) 
+% Spectral radiance [W / m^2 / sr / Hz] = 2 h f lam^-2 / ( e^(h c/(lam kT)) - 1)
+% Spectral radiance [W / m^2 / sr / Hz] = 2 h c lam^-3 / ( e^(h c/(lam kT)) - 1)
 %
 % lam = wavelength, (m)
 % T is black body temperature (Kelvin)
@@ -10,6 +12,11 @@ function U = blackbodyspectrum(lam,tt)
 
 global C HPLANCK KBOLTZMANN;
 
-U1 = 8.*pi.*HPLANCK.*C.*(lam.^-5);
+% spectral energy density:
+% U1 = 8.*pi.*HPLANCK.*C.*(lam.^-5);
+
+% spectral radiance:
+U1 = 2.*HPLANCK.*C.*(lam.^-3);
+
 U2 = exp( HPLANCK.*C./(lam.*KBOLTZMANN.*tt) ) - 1;
 U = U1 ./ U2;
